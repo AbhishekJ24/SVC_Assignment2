7f8848473e9fd37a9a11a5000fc487e0932e9bc8
SanginiMiharia <500094896@stu.upes.ac.in>
2024-11-20 01:37:16 +0530

    Multiply and divide added

--- Calculator.java
+++ Calculator.java
@@ -11,6 +11,18 @@ public class Calculator {
     public static double subtract(double a, double b) {
         return a - b;
     }
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Division
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+            return Double.NaN;
+        }
+        return a / b;
+    }
     // Modulus
    
 
